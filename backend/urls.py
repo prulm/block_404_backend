7b3471458493ffd96fc6afbbf80679f12ba0620d
Yared <jaredyared83@gmail.com>
2024-09-13 06:01:55 +0300

    djoser set up

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
