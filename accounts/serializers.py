b43336f0aab7f1a3b7b3ba827d0d5f121359e2de
Yared <jaredyared83@gmail.com>
2024-09-15 06:37:19 +0300

    added email field to usercreateserializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,4 +6,4 @@ User = get_user_model
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User,
-        fields = ('id', 'phone', 'firstName', 'lastName', 'password', 'profilePicture')
+        fields = ('id', 'phone', 'firstName', 'lastName', 'email', 'password', 'profilePicture')
