d9066fab25b86d72617836f1cd1a52dfb31c3dfc
Yared <jaredyared83@gmail.com>
2024-09-24 05:10:34 +0300

    building model and building attachment models created

--- buildings/models.py
+++ buildings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Building(models.Model):
+    name = models.CharField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    houses = models.IntegerField()
+    floors = models.IntegerField()
+    address = models.CharField(max_length=255)
+
+class Attachment(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='attachments/')
