5d22a8df7d3c26ca72e63a060ca58a6644ab407b
Yared <jaredyared83@gmail.com>
2024-09-27 06:16:24 +0300

    added media url

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 
@@ -7,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
