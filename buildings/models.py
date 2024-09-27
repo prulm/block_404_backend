3a70984fb9c380087782e460145057a5b8a99bdf
Yared <jaredyared83@gmail.com>
2024-09-27 07:56:07 +0300

    added event model

--- buildings/models.py
+++ buildings/models.py
@@ -1,16 +1,37 @@
 from django.db import models
+import houses
 
-class Building(models.Model):
-    name = models.CharField(unique=True)
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Building(TimeStampedModel):
+    name = models.CharField(unique=True, max_length=255)
     description = models.TextField(blank=True, null=True)
     houses = models.IntegerField()
     floors = models.IntegerField()
     address = models.CharField(max_length=255)
 
-class Attachment(models.Model):
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='attachments/')
+    @property
+    def houses(self):
+        pass
 
-class Picture(models.Model):
+class Attachment(TimeStampedModel):
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to="building_pictures")
+    file = models.FileField(upload_to=f'building/{building}/attachments/')
+
+class Picture(TimeStampedModel):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to=f"building/{building}/pictures/")
+
+class Event(TimeStampedModel):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    penality = models.DecimalField(max_digits=10, decimal_places=2)
+    commences = models.DateTimeField()
+    attachment = models.FileField(upload_to=f'building/{building}/events/attachments/')
+
