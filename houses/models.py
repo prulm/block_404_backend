87c270f0916eabac109ebcc526a5ad4527532e53
Yared <jaredyared83@gmail.com>
2024-09-30 05:51:29 +0300

    added status field to house model

--- houses/models.py
+++ houses/models.py
@@ -3,12 +3,19 @@ from accounts.models import UserAccount
 from buildings.models import Building, TimeStampedModel
 
 class House(TimeStampedModel):
+
+    class StatusChoices(models.TextChoices):
+        Unoccupied  = "Unoccupied", "UNOCCUPIED"
+        Owner_Resided = "Owner Resided", "OWNER RESIDED"
+        Rented = "Rented", "RENTED"
+
     owner = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name="my_houses")
     building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="building_houses")
     description = models.TextField()
     floor = models.IntegerField()
     floorCode = models.CharField(max_length=10)
     bedrooms = models.IntegerField()
+    status = models.CharField(choices=StatusChoices, default=StatusChoices.Unoccupied, max_length=50)
 
     class Meta:
         unique_together = ("building", "floor", "floorCode")
