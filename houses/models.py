07979c0c03df9a2f1b9fcfc5c33e88588837c0c4
Yared <jaredyared83@gmail.com>
2024-10-02 05:25:57 +0300

    created installment model to track every installment of a specific payment

--- houses/models.py
+++ houses/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from accounts.models import UserAccount
-from buildings.models import Building, Penality, TimeStampedModel
+from buildings.models import Building, Payment, Penality, TimeStampedModel
 
 class House(TimeStampedModel):
 
@@ -33,12 +33,33 @@ class Resident(TimeStampedModel):
     resident = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name="residences")
     isHead = models.BooleanField(default=False)
     isOwner = models.BooleanField(default=False)
+    isActive = models.BooleanField(default=False)
 
 class HousePenality(TimeStampedModel):
     penality = models.ForeignKey(Penality, on_delete=models.CASCADE)
     house = models.ForeignKey(House, on_delete=models.CASCADE, related_name="penalities")
     is_paid = models.BooleanField(default=False)
 
-class PaymentStatus(TimeStampedModel):
+class HousePayment(TimeStampedModel):
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
-    house = models.ForeignKey(House, on_delete====)
+    house = models.ForeignKey(House, on_delete=models.CASCADE, related_name="payments")
+    
+    @property
+    def total_paid(self):
+        return self.installments.aggregate(total=models.Sum('amount'))['total'] or 0
+
+    @property
+    def payment_progress(self):
+        if self.payment.amount_due == 0:
+            return 100
+        return (self.total_paid / self.payment.amount_due) * 100
+
+    def __str__(self):
+        return f"Payment for {self.house} on {self.payment.deadline}: {'Paid' if self.payment_progress == 100 else 'Not Paid'}"
+
+class Installment(TimeStampedModel):
+    payment = models.ForeignKey(HousePayment, related_name='installments', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Installment of {self.amount} on {self.date_paid}"
