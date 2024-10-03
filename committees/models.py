308cc0885b7267737235b2858f40ed5f19b2c378
Yared <jaredyared83@gmail.com>
2024-10-03 04:24:46 +0300

    created committee model

--- committees/models.py
+++ committees/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-from buildings.models import Building
+from accounts.models import UserAccount
+from buildings.models import Building, TimeStampedModel
 
 
 class Committee(models.Model):
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+class Members(TimeStampedModel):
+    committee = models.ForeignKey(Committee, on_delete=models.CASCADE, related_name="members")
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100, default="Member")
