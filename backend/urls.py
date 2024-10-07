8bc003fb65cbb0901c418e915c330654dea90b71
Yared <jaredyared83@gmail.com>
2024-10-07 06:18:06 +0300

    created an end point to create house

--- backend/urls.py
+++ backend/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('buildings/', include('buildings.urls')),
+    path('houses/', include('houses.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
---
+++ houses/urls.py
@@ -0,0 +1,6 @@
+# from django.urls import path
+# from .views import *
+
+# urlpatterns = [
+#     path('create/', HouseCreateView.as_view(), name='create-house'),
+# ]
