abef388f18b04aa9cd13cb099f465c82d6f96914
Yared <jaredyared83@gmail.com>
2024-10-11 05:42:17 +0300

    created an end point to add members to committee

--- committees/urls.py
+++ committees/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create/', CommitteeCreateView.as_view(), name='create-committee')
+    path('create/', CommitteeCreateView.as_view(), name='create-committee'),
+    path('members/add/',  CommitteeMemberAddView.as_view(), name='add-committee-member'),
+
 ]
--- committees/views.py
+++ committees/views.py
@@ -5,3 +5,10 @@ from .serializers import *
 class CommitteeCreateView(CreateAPIView):
     permission_classes = (permissions.IsAdminUser, )
     serializer_class = CreateCommitteeSerializer
+
+class CommitteeMemberAddView(CreateAPIView):
+    permission_classes = (permissions.IsAdminUser, )
+    serializer_class = MemberSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(committee=self.request.data.get('committee'))
