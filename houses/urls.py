e94f5cc87459747e25a6229871701e4f2eb266fb
Yared <jaredyared83@gmail.com>
2024-10-11 04:28:18 +0300

    created an end point for admin to activate residents

--- houses/urls.py
+++ houses/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create/', HouseCreateView.as_view(), name='create-house'),
     path('me/', HouseListView.as_view(),  name='list-my-houses'),
     path('add_resident/', ResidentAddView.as_view(),  name='add-resident'),
+    path('activate_resident/<pk>/', ResidentActivateView.as_view(), name='activate-resident')
 ]
