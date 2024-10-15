be7b9ac6b0f1cc72f195b3eedb56d5bf635fcbaa
Yared <jaredyared83@gmail.com>
2024-10-15 05:38:50 +0300

    handled attachments and pictures sent along with building

--- buildings/views.py
+++ buildings/views.py
@@ -1,7 +1,30 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.response import Response
 from .serializers import *
 
 class BuildingCreateView(CreateAPIView):
     permission_classes = (permissions.IsAdminUser, )
-    serializer_class =  BuildingSerializer
+    
+    def post(self, *args, **kwargs):
+        if(self.request.data.get('job')):
+            serializer = BuildingSerializer(data=self.request.data.get('building'))
+            if (serializer.is_valid()):
+                serializer.save()
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        if (self.request.data['attachments']):
+            serializer = AttachmentsSerializer(data=self.request.data.get('attachments'))
+            if (serializer.is_valid()):
+                serializer.save(building=Building.objects.last())
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+        if (self.request.data['pictures']):
+            serializer = PicturesSerializer(data=self.request.data.get('pictures'))
+            if (serializer.is_valid()):
+                serializer.save(building=Building.objects.last())
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response("Successful", status=status.HTTP_201_CREATED)
