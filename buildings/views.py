8fe8f90e6566326a9a652bf507c71cc37dab9c63
Yared <jaredyared83@gmail.com>
2024-10-15 05:44:26 +0300

    corrected dict key

--- buildings/views.py
+++ buildings/views.py
@@ -7,21 +7,21 @@ class BuildingCreateView(CreateAPIView):
     permission_classes = (permissions.IsAdminUser, )
     
     def post(self, *args, **kwargs):
-        if(self.request.data.get('job')):
+        if(self.request.data.get('building')):
             serializer = BuildingSerializer(data=self.request.data.get('building'))
             if (serializer.is_valid()):
                 serializer.save()
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        if (self.request.data['attachments']):
+        if (self.request.data.get('attachments')):
             serializer = AttachmentsSerializer(data=self.request.data.get('attachments'))
             if (serializer.is_valid()):
                 serializer.save(building=Building.objects.last())
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             
-        if (self.request.data['pictures']):
+        if (self.request.data.get('pictures')):
             serializer = PicturesSerializer(data=self.request.data.get('pictures'))
             if (serializer.is_valid()):
                 serializer.save(building=Building.objects.last())
