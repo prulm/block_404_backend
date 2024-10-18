8dc70d5aec7c8ba4e91204ff165ee246f1ac99b2
Yared <jaredyared83@gmail.com>
2024-10-18 05:24:15 +0300

    changed reference of resident

--- payments/admin.py
+++ payments/admin.py
@@ -4,14 +4,14 @@ from .models import HousePayment, Payment
 class PaymentAdmin(admin.ModelAdmin):
 	list_display = ('id', 'name', 'description', 'building', 'collector', 'type', 'deadline', 'amount', 'isRecurring', 'recurrence_period', 'penality', 'attachment')
 	list_display_links = ('id', 'building', 'name', 'collector')
-	search_fields = ('building__name', 'collector__firstName', 'collector__lastName', 'name')
+	search_fields = ('building__name', 'collector__user__firstName', 'collector__user__lastName', 'name')
 	list_per_page = 25
 	sortable_by = 'id'
 
 class HousePaymentAdmin(admin.ModelAdmin):
     list_display = ('id', 'house', 'amount', 'paid_by')
     list_display_links = ('id', 'house')
-    search_fields = ('house__owner__firstName', 'house__owner__lastName', 'house__floorCode', 'house__description', 'house__building__name', 'amount', 'paid_by__firstName', 'paid_by__lastName')
+    search_fields = ('house__owner__firstName', 'house__owner__lastName', 'house__floorCode', 'house__description', 'house__building__name', 'amount', 'paid_by__user__firstName', 'paid_by__user__lastName')
     list_per_page = 25
     sortable_by = 'id'
 
