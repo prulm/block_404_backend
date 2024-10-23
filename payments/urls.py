fb7548eaa98067d23430f20c9c8c7eeb3ee5c51d
Yared <jaredyared83@gmail.com>
2024-10-23 04:26:46 +0300

    created an end point to make payments

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('create_payment/', PaymentCreateView.as_view(), name='create-payment'),
-    path('list/<int:pk>/', PaymentListView.as_view(), name='list-buildings'),
-    path('pay/', PaymentListView.as_view(), name='list-buildings'),
+    path('list/<int:pk>/', PaymentListView.as_view(), name='list-payments'),
+    path('pay/', HousePaymentCreateView.as_view(), name='pay'),
 ]
